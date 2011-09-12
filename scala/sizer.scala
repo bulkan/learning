ef98f174b5ff4b9b7ccc73f97fa6590f391d918d
Bulkan Savun Evcimen <bulkan@gmail.com>
2011-09-13 00:22:50 +1000

    Added anchor tag counter
    Regular expressions in Scala is awful and non-intuitive
    Why cant I return a List objects arent like Python/Ruby lists they're typed

--- scala/sizer.scala
+++ scala/sizer.scala
@@ -4,6 +4,13 @@ import scala.actors.Actor._
 
 object PageLoader {
   def getPageSize(url: String) = Source.fromURL(url).mkString.length
+  def getPageSizeWithCount(url: String): List[Int] = {
+    var html = Source.fromURL(url).mkString
+    val size = html.length
+    val reg = """(?i)<a[^>]""".r
+    return List(size, reg.findAllIn(html).size)
+  }
+
 }
 
 val urls = List(
@@ -24,13 +31,17 @@ def getPageSizeConcurrently() = {
   val caller = self
 
   for (url <- urls) {
-    actor { caller ! (url, PageLoader.getPageSize(url)) }
+    actor { caller ! (url, PageLoader.getPageSizeWithCount(url)) }
   }
 
   for(i <- 1 to urls.size) {
     receive {
-      case (url, size) => 
+      case (url, t) => 
+      var l = t.asInstanceOf[List[Int]]
+      val size  = l(0)
+      val anchors = l(1)
       println("Size for " + url + ": " + size)
+      println("Has " + ": " + anchors + " anchor tags")
     }
   }
 }
@@ -39,8 +50,5 @@ println("Concurrent run")
 timeMethod { getPageSizeConcurrently }
 
 /*
-
-  * Add a count of number of links on page
-  * Bonus: make the sizer follow all the links on a give page and load them as well.
-
+  Bonus: make the sizer follow all the links on a give page and load them as well.
 */
