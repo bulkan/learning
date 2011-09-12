55f408768afa54a39896febd83050816a11918d4
Bulkan Evcimen <bulkan@gmail.com>
2011-09-12 16:44:01 +1000

    using foldLeft with an Array

--- scala/folding.scala
+++ scala/folding.scala
@@ -1,8 +1,17 @@
 object Main {
   def main(args: Array[String]) {
-    var myStrings = List("bulkan", "savun", "evcimen")
-    var stringSum = myStrings.foldLeft(0)((sum, value) => sum + value.length)
-    println("Sum of strings: " + stringSum)
+    var myStrings = Array("bulkan", "savun", "evcimen")
 
+    if (args.length > 0) {
+      myStrings = args
+    }
+
+    print("Given strings: ")
+    myStrings.foreach(s => print(s + ", "))
+
+    // Using foldLeft to calculate sum strings in List
+    var stringSum = myStrings.foldLeft(0)((sum, value) => sum + value.length)
+    println()
+    println("Sum of strings: " + stringSum)
   }
 }
