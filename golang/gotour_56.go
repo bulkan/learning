0f637e4392ab10d17a156daf41cc749664ef631b
Bulkan Evcimen <bulkan@gmail.com>
2013-01-16 22:44:07 +1100

    solved exercise 56

--- golang/gotour_56.go
+++ golang/gotour_56.go
@@ -5,6 +5,13 @@ import (
         "math"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+    return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+
 func Avg(a, b float64) float64 {
     return ((a + b) / 2.0)
 }
@@ -18,16 +25,21 @@ func good_enough(guess, x float64) bool {
     return d < 0.001
 }
 
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, error) {
+    if x < 0 {
+        return 0, ErrNegativeSqrt(x)
+    }
+
     guess := float64(1)
     for i := 0; i < 10; i++ {
         guess = Improve(float64(guess), x)
 
     }
-    return guess
+    return guess, nil
 }
 
 func main() {
     fmt.Println(Sqrt(2))
     fmt.Println(math.Sqrt(2))
+    fmt.Println(Sqrt(-2))
 }
