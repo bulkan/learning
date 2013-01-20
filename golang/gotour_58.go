10ee1e42115779d79490f09c2b38e76572dae35d
Bulkan Evcimen <bulkan@gmail.com>
2013-01-20 21:36:47 +1100

    solved images

--- golang/gotour_58.go
+++ golang/gotour_58.go
@@ -2,25 +2,35 @@ package main
 
 import (
     "image"
+    "image/color"
     "code.google.com/p/go-tour/pic"
 )
 
 
-type Image struct{}
+// had to get help on this one was really confused with the instruction
+// of returning color.RGBAModel in ColorModel() 
+// https://github.com/guanqun/gotour-answers/blob/master/images.go
 
-func Pic(dx, dy int) [][]uint8 {
-    s := make([][]uint8, dy)
-
-    for y := 0; y < dy; y++ {
-        s[y] = make([]uint8, dx)
-        for x := 0; x < dx ; x++ {
-            s[y][x] = uint8(y^x * y+4/2 )
-        }
-    }
-    return s
+type Image struct{
+    width int
+    height int
 }
 
+func (i Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, i.width, i.height)
+}
+
+func (i Image) At(x, y int) color.Color {
+    v := uint8((x^y * y+4/ 2))
+    return color.RGBA{v, v, 255, 255}
+}
+
+
 func main() {
-    m := Image{}
+    m := Image{100, 100}
     pic.ShowImage(m)
 }
