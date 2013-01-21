7fb9d9f7e482da5b07ec31d8d977216253b71e2d
Bulkan Evcimen <bulkan@gmail.com>
2013-01-21 22:58:05 +1100

    wrote Same function go channels are cool

--- golang/gotour_67.go
+++ golang/gotour_67.go
@@ -9,6 +9,7 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
+    // Exhausts left side of the tree first
     if t.Left != nil {
         Walk(t.Left, ch)
     }
@@ -18,19 +19,32 @@ func Walk(t *tree.Tree, ch chan int) {
         Walk(t.Right, ch)
     }
 
-
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-/*func Same(t1, t2 *tree.Tree) bool*/
+func Same(t1, t2 *tree.Tree) bool {
+    ch1 := make(chan int)
+    ch2 := make(chan int)
+
+    go Walk(t1, ch1)
+    go Walk(t2, ch2)
+
+    for i := 1; i <= 10; i++ {
+        v1 := <-ch1
+        v2 := <-ch2
+        if v1 != v2 {
+            return false
+        }
+    }
+
+    return true
+}
 
 func main() {
-    ch := make(chan int)
-    t := tree.New(1)
-    fmt.Println(t.String())
-    go Walk(t, ch)
-    for i := 1; i <= 10; i++  {
-        fmt.Println(<-ch)
-    }
+    t1 := tree.New(1)
+    t2 := tree.New(1)
+    fmt.Println(Same(t1, t2))
+    t3 := tree.New(2)
+    fmt.Println(Same(t1, t3))
 }
