55cd33d27be707a13d8878d485e0bed121db472b
Bulkan Evcimen <bulkan@gmail.com>
2013-01-21 21:54:15 +1100

    i cracked the code

--- golang/gotour_59.go
+++ golang/gotour_59.go
@@ -11,12 +11,33 @@ type rot13Reader struct {
 }
 
 func (rot *rot13Reader) Read(p []byte) (n int, err error) {
-    return rot.r.Read(p)
+    rn, err := rot.r.Read(p)
+
+    if err != nil {
+        return rn, err
+    }
+
+    // loop over each char that is read
+    for i := 0; i < rn; i++ {
+        switch {
+            case p[i] >= 'A' && p[i] <= 'Z':
+                p[i] = 'A' + (p[i]-'A'+13)%26
+            case p[i] >= 'a' && p[i] <= 'z':
+                p[i] = 'a' + (p[i]-'a'+13)%26
+            }
+        }
+
+    return rn, err
 }
 
 
 func main() {
-    s := strings.NewReader("Lbh penpxrq gur pbqr!")
+    s := strings.NewReader("Lbh penpxrq gur pbqr!\n")
     r := rot13Reader{s}
+
+    /*The following two line
+    buf := make([]byte, 5)
+    /*t.Read(buf)
+    fmt.Println(buf)*/
     io.Copy(os.Stdout, &r)
 }
